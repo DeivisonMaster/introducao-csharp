b49a4c629013be58100d6d16eb93e8af130c9091
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-03-16 19:04:55 -0400

    arrays e arraylist

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object objeto1;
+
+            int a1 = 12;
+            int a2 = 4;
+
+            object o = a1;
+
+            //Console.WriteLine(b.GetType());
+            //Console.ReadKey();
+
+            int c = (int) o;
+
+            // Conversão explicita com acesso a métodos e atributos
+            string d = ((int) o).ToString();
+
+            Type tipo = d.GetType();
+
+            Console.WriteLine(tipo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vetores/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // array de unica dimensão
+                int[] a1 = new int[3];
+
+                a1[0] = 12;
+                a1[1] = 3;
+                a1[2] = 31;
+                //a1[3] = 4;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Array fora do indice" + e);
+                Console.ReadKey();
+            }
+
+
+            int[] a2 = new int[] {10,55,31,533,00};
+
+            string[] a3 = {"brazil", "usa", "china"};
+
+            int tamanhoArray = a3.Length;
+
+            // array multidimensional
+            int[,] ma1 = new int[2, 3];
+
+
+            ICollection lista1 = new ArrayList();
+            ICollection lista2 = new List<Object>();
+
+            IList lista3 = new ArrayList();
+            IList lista4 = new List<Object>();
+
+
+            Console.WriteLine(tamanhoArray);
+            Console.ReadKey();
+        }
+    }
+}
