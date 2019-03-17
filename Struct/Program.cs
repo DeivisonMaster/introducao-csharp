084a54920d198ada6a289290a321b2efbe23e17d
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-03-17 19:18:19 -0400

    struct C#

---
+++ Struct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+
+        public struct Livro
+        {
+            public string titulo;
+            public string autor;
+            public int isbn;
+        }
+
+        static void Main(string[] args)
+        {
+            Livro livro1 = new Livro();
+
+            livro1.autor = "Andre Homes";
+            livro1.titulo = "A cabana";
+            livro1.isbn = 569898;
+
+
+        }
+    }
+}
