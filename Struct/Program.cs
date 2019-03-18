be527e0c51c355c17dd535054052170f779815fc
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-03-17 20:42:41 -0400

    foreach C#

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,12 +20,32 @@ namespace Struct
         static void Main(string[] args)
         {
             Livro livro1 = new Livro();
-
             livro1.autor = "Andre Homes";
             livro1.titulo = "A cabana";
             livro1.isbn = 569898;
 
+            Livro livro2 = new Livro();
+            livro2.autor = "Marisa Monte";
+            livro2.titulo = "O amor nunca morre";
+            livro2.isbn = 123812;
 
+
+            List<Livro> livros = new List<Livro>();
+            livros.Add(livro1);
+            livros.Add(livro2);
+
+            foreach (Livro livro in livros)
+            {
+                Console.WriteLine("Autor: " + livro.autor);
+                Console.WriteLine("Titulo: " + livro.titulo);
+                Console.WriteLine("ISBN: " + livro.isbn);
+                Console.WriteLine("************************");
+            }
+
+            Livro liv = livros.ElementAt<Livro>(1);
+            Console.WriteLine("Autor: " + liv.autor);
+
+            Console.ReadKey();
         }
     }
 }
