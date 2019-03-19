75571a7002f60d26d4709353f43afb6d9b7a342a
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-03-18 22:10:22 -0400

    anonymous type

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tornando variavel do tipo numerica nula em C#
+            int? i = null;
+            string s = null;
+
+
+            // Anonymous Type
+            var valores = new[] {
+                new { idade = 18, nome = "Luis" },
+                new { idade = 22, nome = "Alberto" }
+            };
+
+
+            foreach (var valor in valores)
+            {
+                Console.WriteLine("Idade: " + valor.idade + " " + "Nome: " + valor.nome);
+            }
+            Console.ReadKey();
+        }
+    }
+}
